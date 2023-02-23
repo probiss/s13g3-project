313131a2118ca903b3d045f773f5dd5a9d27bc32
probiss <bulut.cihad@gmail.com>
2023-02-23 10:52:34 +0300

    message

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ server.use(getLog);
 
 //3- Routing örneği
 server.get("/", (req,res)=> {
-    res.status(200).json({statusCode: 200, message: "Hey, server is up and running..."});
+    res.status(200).json({statusCode: 200,  message: req.MESSAGE || "Hey, server is up and running..."});
 } )
 
 /*
